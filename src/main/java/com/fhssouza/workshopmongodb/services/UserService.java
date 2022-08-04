9be7bd2cc873cafb5f0870ad5af854a2b3b01935
fhssouza <fhssouza@gmail.com>
2022-08-04 20:46:53 -0300

    Atualização de usuário com PUT

--- src/main/java/com/fhssouza/workshopmongodb/services/UserService.java
+++ src/main/java/com/fhssouza/workshopmongodb/services/UserService.java
@@ -13,11 +13,11 @@ import com.fhssouza.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
 
@@ -25,16 +25,27 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		userRepository.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
