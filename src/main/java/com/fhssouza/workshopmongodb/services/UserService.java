8afbb2792e950aed06ece96be0f6bf68c181f6e8
fhssouza <fhssouza@gmail.com>
2022-08-04 19:29:12 -0300

    Obtendo um usuário por id

--- src/main/java/com/fhssouza/workshopmongodb/services/UserService.java
+++ src/main/java/com/fhssouza/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.fhssouza.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fhssouza.workshopmongodb.domain.User;
 import com.fhssouza.workshopmongodb.repository.UserRepository;
+import com.fhssouza.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,8 @@ public class UserService {
 		return userRepository.findAll();
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
