9aef69385a16f65a4f6d4ede69467a300c051093
fhssouza <fhssouza@gmail.com>
2022-08-04 17:47:06 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/fhssouza/workshopmongodb/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.fhssouza.workshopmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.fhssouza.workshopmongodb.domain.User;
+import com.fhssouza.workshopmongodb.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
