0453efdad05660c4de1313eb6df2778597d8cc7e
fhssouza <fhssouza@gmail.com>
2022-08-08 16:43:57 -0300

    Obtendo um post por id

---
+++ src/main/java/com/fhssouza/workshopmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.fhssouza.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fhssouza.workshopmongodb.domain.Post;
+import com.fhssouza.workshopmongodb.repository.PostRepository;
+import com.fhssouza.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
