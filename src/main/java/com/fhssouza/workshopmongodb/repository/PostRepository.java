c90063ef842d16cba9da85235e1bcca13264c1ac
fhssouza <fhssouza@gmail.com>
2022-08-08 17:51:34 -0300

    Consulta simples com @Query

--- src/main/java/com/fhssouza/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/fhssouza/workshopmongodb/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.fhssouza.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fhssouza.workshopmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
